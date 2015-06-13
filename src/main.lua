98525d1b09d1ae39b086c2dd0e7e937b9e29d3a1
Jozef Mitro <jozef.mitro.sk@gmail.com>
2015-06-13 09:49:40 +0200

    Add bouncing to the echoes

--- src/main.lua
+++ src/main.lua
@@ -9,12 +9,12 @@ function love.load(args)
   player.acceleration = 1.0e2
   player.drag = 2.5e-1
   player.size = 32
+  player.color = {255, 255, 255, 255}
   player.echoTimer = 0
   player.echoCooldown = 1
   
   ECHO_GROWTH = 320
-  ECHO_MAX_SIZE = 1280
-  
+  ECHO_MAX_SIZE = 1280 
   echoes = {}
   
   prey = {} 
@@ -25,6 +25,7 @@ function love.load(args)
   prey.acceleration = 1.0e2
   prey.drag = 2.5e-1
   prey.size = 16
+  prey.color = {0, 255, 0, 255}
   
   predator = {} 
   predator.x = 600
@@ -34,6 +35,7 @@ function love.load(args)
   predator.acceleration = 1.0e2
   predator.drag = 2.5e-1
   predator.size = 64
+  predator.color = {255, 0, 0, 255}
 end
 
 function love.update(dt)
@@ -60,14 +62,7 @@ function love.update(dt)
   player.y = player.y + player.dy * dt
   
   if love.keyboard.isDown(" ") and player.echoTimer == 0 then
-    local newEcho = {}
-    newEcho.x = player.x
-    newEcho.y = player.y
-    newEcho.size = player.size
-    newEcho.color = {255, 255, 255, 255}
-    
-    echoes[newEcho] = true
-    
+    emitEcho(player)    
     player.echoTimer = player.echoCooldown
   end
   
@@ -78,8 +73,22 @@ function love.update(dt)
     -- Update alpha
     echo.color[4] = math.max(255 - (echo.size / ECHO_MAX_SIZE) * 255, 0)
     
+    if echo.owner == player then
+      if circlesCollide(echo, prey) and
+        not echo.echoedFrom[prey] then
+        emitEcho(prey)
+        echo.echoedFrom[prey] = true
+      end
+      
+      if circlesCollide(echo, predator) and
+        not echo.echoedFrom[predator]  then
+        emitEcho(predator)
+        echo.echoedFrom[predator] = true
+      end
+    end
+    
     if echo.size > ECHO_MAX_SIZE then
-      echoes[echo] = false
+      echoes[echo] = nil
     end
   end
 end
@@ -88,15 +97,15 @@ function love.draw()
   love.graphics.setBackgroundColor(0, 0, 32)
   
   -- Draw prey
-  love.graphics.setColor(0, 255, 0)
+  love.graphics.setColor(prey.color)
   love.graphics.circle("fill", prey.x, prey.y, prey.size / 2)
   
   -- Draw player
-  love.graphics.setColor(255, 255, 255)
+  love.graphics.setColor(player.color)
   love.graphics.circle("fill", player.x, player.y, player.size / 2)
   
   -- Draw predator
-  love.graphics.setColor(255, 0, 0)
+  love.graphics.setColor(predator.color)
   love.graphics.circle("fill", predator.x, predator.y, predator.size / 2)
   
   -- Draw echoes
@@ -117,4 +126,33 @@ function circlesOverlap(circle1, circle2)
   local distanceSquared = math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2)
   
   return distanceSquared < math.pow(r2 - r1, 2)
+end
+
+function circlesCollide(circle1, circle2)
+  local x1 = circle1.x
+  local x2 = circle2.x
+  local y1 = circle1.y
+  local y2 = circle2.y
+  local r1 = circle1.size / 2
+  local r2 = circle2.size / 2
+  
+  local distanceSquared = math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2)
+  
+  return distanceSquared < math.pow(r2 + r1, 2)
+end
+
+function emitEcho(entity)
+  local newEcho = {}
+  newEcho.x = entity.x
+  newEcho.y = entity.y
+  newEcho.size = entity.size
+  newEcho.color = {}
+  newEcho.owner = entity
+  newEcho.echoedFrom = {}
+  
+  for partKey, colorPart in pairs(entity.color) do
+    newEcho.color[partKey] = colorPart
+  end
+    
+  echoes[newEcho] = true 
 end
